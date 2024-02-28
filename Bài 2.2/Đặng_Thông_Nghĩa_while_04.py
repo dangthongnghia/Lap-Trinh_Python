89b1c55d466bb3d66d71a2e0b124a4f90583d8ee
Đặng Thông Nghĩa <141739914+dangthongnghia@users.noreply.github.com>
2024-02-28 11:52:58 +0700

    Đặng Nghĩa

--- Bài 2.2/Đặng_Thông_Nghĩa_while_04.py
+++ Bài 2.2/Đặng_Thông_Nghĩa_while_04.py
@@ -1,8 +1,8 @@
 # Khởi tạo biến tích và biến số chẵn
 tich = 1
 le = 1
-while tich * chan <= 50:
- tich *= chan
+while tich * le <= 50:
+ tich *= le
  le += 1
 print(f'Tích của các số le là: {tich}')
 
