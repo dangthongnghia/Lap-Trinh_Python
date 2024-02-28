1b614000fcf2451deb56cdbd3d545eed1e2a7e1c
Đặng Thông Nghĩa <141739914+dangthongnghia@users.noreply.github.com>
2024-02-28 11:38:15 +0700

    Đặng Nghĩa

---
+++ Đặng Thông Nghĩa_if_02.py
@@ -0,0 +1,12 @@
+thang = int(input('Nhập tháng: '))
+nam = int(input('Nhập năm:'))
+if thang in (1,3,5,7,8,10,12):
+    print(f'Tháng {thang} có 31 ngày')
+elif thang in (4,6,9,11):
+    print(f'Tháng {thang} có 30 ngày')
+if thang == (2):
+
+ if(nam % 4 == 0 and nam % 100 !=0) or nam % 400 == 0:
+      print(f'Tháng {thang} có 29 ngày:')
+ else:
+      print(f'Tháng {thang} có 28 ngày:')
