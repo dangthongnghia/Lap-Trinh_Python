820c11ef8693828083824b243fb652bed92b0207
Đặng Thông Nghĩa <141739914+dangthongnghia@users.noreply.github.com>
2024-02-28 10:35:23 +0700

    DangNghia

---
+++ Bài 2.2/Đặng_Thông_Nghĩa_while_01.py
@@ -0,0 +1,11 @@
+# Khởi tạo biến tổng và biến số chẵn
+tong = 0
+so_chan = 2
+
+# Lặp cho đến khi tổng vượt quá 50
+while tong + so_chan <= 100:
+    tong += so_chan
+    so_chan += 2
+
+print(f"Tổng các số chẵn là: {tong}")
+
