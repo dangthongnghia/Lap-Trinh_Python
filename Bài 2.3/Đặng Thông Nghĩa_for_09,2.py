c18450fb84a5adfa20fcc1a94a7cc47f24d42d14
Dang Thong Nghia <dangnghia2385@gmail.com>
2024-03-10 20:15:34 +0700

    update

---
+++ Bài 2.3/Đặng Thông Nghĩa_for_09,2.py
@@ -0,0 +1,31 @@
+luachon = int(input('Nhập vào bảng cửu chương bạn muốn in ra:'))
+
+if(luachon > 1 and  luachon < 10):
+  print(f'Bảng cửu chương {luachon} là:') 
+else:
+    print('Giá trị không hợp lệ')  
+for j in range(1,10):
+    
+    if(luachon == 2):
+        print(f"{luachon} x {j} = {luachon*j}")
+    elif(luachon == 3):
+        print(f"{luachon} x {j} = {luachon*j}")
+    elif(luachon == 4):
+        print(f"{luachon} x {j} = {luachon*j}")
+    elif(luachon == 5):
+        print(f"{luachon} x {j} = {luachon*j}")
+    elif(luachon == 6):
+        print(f"{luachon} x {j} = {luachon*j}") 
+    elif(luachon == 7):
+        print(f"{luachon} x {j} = {luachon*j}")
+    elif(luachon == 8):
+        print(f"{luachon} x {j} = {luachon*j}")
+    elif(luachon == 9):
+        print(f"{luachon} x {j} = {luachon*j}")
+    
+    
+
+
+
+
+
