f608993b5a460c66661e8eb23cb1e773ea285a07
Đặng Thông Nghĩa <141739914+dangthongnghia@users.noreply.github.com>
2024-03-12 13:17:46 +0700

    commit

---
+++ Đặng Thông Nghĩa_str/Đặng Thông Nghĩa_str_18.py
@@ -0,0 +1,15 @@
+P = 'Python '
+f = 'For '
+e = 'Everyone'
+
+phim_tat = P+f+e
+print(phim_tat)
+
+# Gán chuỗi cho một biến
+my_string = "Python For Everyone"
+
+# Tạo phím tắt cho chuỗi
+shortcut = my_string[:3]  # Lấy ba ký tự đầu tiên
+
+# In phím tắt
+print("Phím tắt:", shortcut)
